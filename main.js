8ec9ab2ed535bcce8b5fa8a2270f07938c8f5d1a
Hamza77html <125993127+Hamza77html@users.noreply.github.com>
2023-04-13 05:02:54 +0200

    G

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 fetch('https://api.aladhan.com/v1/timingsByCity/13-04-2023?city=cairo&country=egypt&method=8')
-  .then(response => response.json())
-    .then(data => document.write(data))
+  .then(response => response.json(response))
+    .then(data => console.log(data))
       .catch(error => console.error(error));
 
