afb84ce5ea3dbce40295801103aa95cccca5ecb7
Hamza77html <125993127+Hamza77html@users.noreply.github.com>
2023-04-13 05:11:57 +0200

    غ

--- main.js
+++ main.js
@@ -1,5 +1,21 @@
-fetch('https://api.aladhan.com/v1/timingsByCity/13-04-2023?city=cairo&country=egypt&method=8')
-  .then(response => response.json(response))
-    .then(data => console.log(data))
-      .catch(error => console.error(error));
+fetch('https://example.com/api/data')
+  .then(response => response.json())
+    .then(data => {
+        // Do something with the data, such as assigning it to a variable
+            const apiData = data;
+              })
+                .catch(error => console.error(error));
+                const data = {
+                    name: 'John Doe',
+                      email: 'johndoe@example.com'
+                      };
+
+                      axios.post('https://example.com/api/user', data)
+                        .then(response => {
+                            // Do something with the response data, such as assigning it to a variable
+                                const responseData = response.data;
+                                  })
+                                    .catch(error => console.error(error));
+                                    
+                }
 
