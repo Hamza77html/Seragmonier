a9622e9f95fa7ad775573dcc14d3c01a08586d58
Hamza77html <125993127+Hamza77html@users.noreply.github.com>
2023-04-15 05:09:13 +0200

    ع

--- main.js
+++ main.js
@@ -1,19 +1,69 @@
+const prayerTimingsForm = document.getElementById('prayer-timings-form');
 const prayerTimings = document.getElementById('prayer-timings');
 
-fetch('https://api.aladhan.com/v1/timingsByCity/15-04-2023?city=cairo&country=egypt&method=8')
-  .then(response => response.json())
-    .then(data => {
-        const date = document.createElement('h2');
-            date.textContent = data.date.readable;
-                prayerTimings.appendChild(date);
+prayerTimingsForm.addEventListener('submit', event => {
+  event.preventDefault();
 
-                    for (const prayerName in data.timings) {
-                          const prayerTime = document.createElement('p');
-                                prayerTime.textContent = `${prayerName}: ${data.timings[prayerName]}`;
-                                      prayerTimings.appendChild(prayerTime);
-                                          }
-                                            })
-                                              .catch(error => console.error(error));
+    const city = document.getElementById('city').value;
+      const country = document.getElementById('country').value;
+        const method = document.getElementById('method').value;
+
+          const endpointUrl = `https://api.aladhan.com/v1/timingsByCity/${getCurrentDate()}?city=${city}&country=${country}&method=${method}`;
+
+            fetch(endpointUrl)
+                .then(response => response.json())
+                    .then(data => {
+                          const date = document.createElement('h2');
+                                date.textContent = data.date.readable;
+                                      prayerTimings.innerHTML = '';
+                                            prayerTimings.appendChild(date);
+
+                                                  const table = document.createElement('table');
+                                                        const tableHeader = document.createElement('thead');
+                                                              const tableHeaderRow = document.createElement('tr');
+                                                                    const prayerNames = ['Fajr', 'Dhuhr', 'Asr', 'Maghrib', 'Isha'];
+
+                                                                          prayerNames.forEach(prayerName => {
+                                                                                  const headerCell = document.createElement('th');
+                                                                                          headerCell.textContent = prayerName;
+                                                                                                  tableHeaderRow.appendChild(headerCell);
+                                                                                                        });
+
+                                                                                                              tableHeader.appendChild(tableHeaderRow);
+                                                                                                                    table.appendChild(tableHeader);
+
+                                                                                                                          const tableBody = document.createElement('tbody');
+                                                                                                                                const timingsRow = document.createElement('tr');
+
+                                                                                                                                      prayerNames.forEach(prayerName => {
+                                                                                                                                              const timingCell = document.createElement('td');
+                                                                                                                                                      timingCell.textContent = data.data.timings[prayerName.toLowerCase()];
+                                                                                                                                                              timingsRow.appendChild(timingCell);
+                                                                                                                                                                    });
+
+                                                                                                                                                                          tableBody.appendChild(timingsRow);
+                                                                                                                                                                                table.appendChild(tableBody);
+                                                                                                                                                                                      prayerTimings.appendChild(table);
+                                                                                                                                                                                          })
+                                                                                                                                                                                              .catch(error => {
+                                                                                                                                                                                                    prayerTimings.textContent = `Error: ${error.message}`;
+                                                                                                                                                                                                        });
+                                                                                                                                                                                                        });
+
+                                                                                                                                                                                                        function getCurrentDate() {
+                                                                                                                                                                                                          const currentDate = new Date();
+                                                                                                                                                                                                            const year = currentDate.getFullYear();
+                                                                                                                                                                                                              const month = (currentDate.getMonth() + 1).toString().padStart(2, '0');
+                                                                                                                                                                                                                const day = currentDate.getDate().toString().padStart(2, '0');
+                                                                                                                                                                                                                  return `${day}-${month}-${year}`;
+                                                                                                                                                                                                                  }
+                                                                                                                                                                                                                  
+            
+                    
+                          
+                                  
+                                          
+                                      
                                               
 
         
